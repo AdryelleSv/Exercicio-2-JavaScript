63493666bcabb23657669550ebf4beb3d35363ab
Adryelle Silva <119129134+AdryelleSv@users.noreply.github.com>
2023-04-12 20:35:41 -0300

    Add files via upload

---
+++ Interação/index.js
@@ -0,0 +1,8 @@
+confirm('quer ouvir uma curiosidade?')
+confirm('ok mas é segredo ta?')
+
+
+function clicou(){
+    alert('A batata possui bastante água, vitamina B1, Vitamina C e sais minerais, principalmente ferro, magnésio e potássio e ....')
+    alert('o que? eu nao peguei isso da internet...')
+}
